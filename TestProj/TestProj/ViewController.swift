d5ca528bd8f7b46ac24726caedecc887fc0eb4e3
peleg.aran <=peleg.aran@gmail.com>
2014-12-25 14:41:33 +0200

    Peleg commit

--- TestProj/TestProj/ViewController.swift
+++ TestProj/TestProj/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        println("test peleg")
+        println("test peleg2")
     }
 
     override func didReceiveMemoryWarning() {
