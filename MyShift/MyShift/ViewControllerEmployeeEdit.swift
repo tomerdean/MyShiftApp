0f2be1c125a6ae7c69965a1ac55119a4ca02d589
tomerdean <nitaidean@Toms-MacBook-Air-3.local>
2014-12-26 01:46:40 +0200

    ss

--- MyShift/MyShift/ViewControllerEmployeeEdit.swift
+++ MyShift/MyShift/ViewControllerEmployeeEdit.swift
@@ -13,28 +13,51 @@ class ViewControllerEmployeeEdit: UIViewController {
     var origColor = UIColor(red: 0xef/255, green: 0xef/255, blue: 0xef/255, alpha: 1.0)
     var newColor = UIColor(red: 0xcd/255, green: 0xff/255, blue: 0xd1/255, alpha: 1.0)
     
+    var storeClicks = [[Bool]]()
     
     @IBOutlet weak var sundayMorningButton: UIButton!
+    
+    @IBOutlet weak var sundayEveningButton: UIButton!
+    
+    @IBOutlet weak var mondayMorningButton: UIButton!
+    
+    @IBOutlet weak var mondayEveningButton: UIButton!
+    
+    @IBOutlet weak var tuesdayMorningButton: UIButton!
+    
+    @IBOutlet weak var tuesdayEveningButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func resetValues() {
+        
+        for i in 1...7 {
+            for e in 1...2 {
+                storeClicks[i][e] = false
+            }
+        }
+        
+    }
+    
     @IBAction func sundayMorning(sender: UIButton) {
 
         
         println("color is currently \(sundayMorningButton.backgroundColor)")
         sundayMorningButton.backgroundColor = newColor
-        /*
+        
         // item is not clicked
-        if (saveClicks[10] == false) {
-            saveClicks.insert(true, atIndex: 10)
+        if (storeClicks[1][1] != true) {
+            storeClicks[1][1] = true
             sundayMorningButton.backgroundColor = newColor
         }
         else {
-            saveClicks[10] = false;
+            storeClicks[1][1] = false;
             sundayMorningButton.backgroundColor = origColor
-        }*/
+        }
     }
     
     
