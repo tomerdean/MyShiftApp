6a42cb56dac5b619edb997469d2a5740496d80e7
omripd <omri@Neals-MacBook-Air.local>
2014-12-25 15:06:36 +0200

    de

--- TestProj/TestProj/ViewController.swift
+++ TestProj/TestProj/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        println("test peleg 1 2 3 4 5peleg aran")
+        println("test peleg 1 2 3 4 5peleg aran omri")
     }
 
     override func didReceiveMemoryWarning() {
