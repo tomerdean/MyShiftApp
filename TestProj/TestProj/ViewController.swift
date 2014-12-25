4856c2b6b3091586d7f351d8bb1af8a6357a7e12
tomerdean <nitaidean@Toms-MacBook-Air-3.local>
2014-12-25 14:32:36 +0200

    Added pro files

---
+++ TestProj/TestProj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestProj
+//
+//  Created by Tom Dean on 12/25/14.
+//  Copyright (c) 2014 TeamTop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
