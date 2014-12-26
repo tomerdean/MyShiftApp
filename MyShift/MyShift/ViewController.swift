8b2729a03e4d1f088b1d4b333ea9e9433d5afb84
peleg.aran <=peleg.aran@gmail.com>
2014-12-26 10:36:43 +0200

    lll

--- MyShift/MyShift/ViewController.swift
+++ MyShift/MyShift/ViewController.swift
@@ -86,4 +86,8 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func backgroundTap(sender: UIControl) {
+        txtUsername.resignFirstResponder()
+    }
 }
